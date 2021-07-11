22d2b934115f72216b60e7d3c8ccd35c3986d1bf
Henri Heyden <44402515+WiiXLinux@users.noreply.github.com>
2021-07-11 21:31:33 +0200

    Add files via upload

---
+++ src/bin/Main.java
@@ -0,0 +1,10 @@
+package bin;
+
+import lib.JCMD;
+
+public class Main {
+    public static void main(String[] args) {
+        JCMD.execute("Random Gibberish", "This text doesn't matter");
+    }
+
+}
