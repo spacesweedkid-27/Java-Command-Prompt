59abf2bdd3803829008f2fe43b12ffd46630c3a6
Henri Heyden <44402515+WiiXLinux@users.noreply.github.com>
2021-07-11 21:36:26 +0200

    Add files via upload

--- lib/JCMD.java
+++ lib/JCMD.java
@@ -1,10 +1,28 @@
 package lib;
 
-import util.IllegalArgumentException;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+class IllegalArgumentException extends Exception {
+    public IllegalArgumentException() {
+        super();
+    }
+
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+
+    public IllegalArgumentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IllegalArgumentException(Throwable cause) {
+        super(cause);
+    }
+}
 
 public class JCMD {
 
