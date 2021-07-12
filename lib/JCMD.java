b277f65510ae6da6bb3b6d7562748a801d48f5bc
Henri Heyden <44402515+WiiXLinux@users.noreply.github.com>
2021-07-12 11:04:42 +0200

    Update JCMD.java

--- lib/JCMD.java
+++ lib/JCMD.java
@@ -1,71 +1,63 @@
 package lib;
 
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 class IllegalArgumentException extends Exception {
-    public IllegalArgumentException() {
-        super();
-    }
+
 
     public IllegalArgumentException(String message) {
         super(message);
     }
 
-    public IllegalArgumentException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public IllegalArgumentException(Throwable cause) {
-        super(cause);
-    }
 }
 
 public class JCMD {
+    public static HashMap<String, String> commands = new HashMap<String, String>();
 
+    public JCMD() {
+        commands.put("C", "C:\\Windows\\System32\\");
+        commands.put("COMMAND", "C:\\Windows\\System32\\");
+        commands.put("CMD", "C:\\Windows\\System32\\cmd.exe");
+        commands.put("P", "C:\\Program Files\\");
+        commands.put("PROGRAM FILES", "C:\\Program Files\\");
+        commands.put("P86", "C:\\Program Files (x86)\\");
+        commands.put("PROGRAM FILES (X86)", "C:\\Program Files (x86)\\");
+        commands.put("", "");
+    }
 
     public static void execute(String args, String command) {
         try {
-            String command_end = command;
+            if (commands.get(args) != null && !(args.equals("H") || args.equals("HELP"))) {
 
-            if (args.equals("H") || args.equals("HELP")) {
+                String real_command = commands.get(args) + command;
+                System.out.println(real_command);
+
+                Process process = Runtime.getRuntime().exec(real_command);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                reader.close();
+            } else if (args.equals("H") || args.equals("HELP")) {
                 System.out.println(
                         "Java Command Prompt v.1.1\n" +
-                        "\n" +
-                        "H or HELP:              Displays this message\n" +
-                        "C or COMMAND:           Use a windows command located in System32\n" +
-                        "P or PROGRAM FILES      Use a command located in Program Files\n" +
-                        "P86 or PROGRAM FILES    Use a command located in Program Files (86)");
-            } else
-            if (args.equals("C") || args.equals("COMMAND")) {
-                command = "C:\\Windows\\System32\\";
-            } else if (args.equals("CMD")) {
-                command = "C:\\Windows\\System32\\cmd.exe";
-            } else if (args.equals("P") || args.equals("PROGRAM FILES")) {
-                command = "C:\\Program Files\\";
-            } else if (args.equals("P86") || args.equals("PROGRAM FILES x86")) {
-                command = "C:\\Program Files (x86)\\";
-            }  else if (args.equals("") || args.equals(null)) {
+                                "\n" +
+                                "H or HELP:              Displays this message\n" +
+                                "C or COMMAND:           Use a windows command located in System32\n" +
+                                "P or PROGRAM FILES      Use a command located in Program Files\n" +
+                                "P86 or PROGRAM FILES    Use a command located in Program Files (86)");
+
 
             } else {
-                throw new IllegalArgumentException("\"unknown argument: '\" + args + \"'\"");
-
+                throw new IllegalArgumentException("unknown argument '" + args + "'");
             }
-            command = command + command_end;
-
-            Process process = Runtime.getRuntime().exec(command);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            reader.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         } catch (IllegalArgumentException e) {
